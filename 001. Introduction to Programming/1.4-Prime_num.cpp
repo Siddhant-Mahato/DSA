692b5bf767bf64b9fc8048cfe0330ab5307c0ddf
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-06-29 00:41:28 +0530

    Introduction to Programming

---
+++ 001. Introduction to Programming/1.4-Prime_num.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n ;
+    cout<<" enter the value of n" <<endl;
+    cin >> n;
+
+    bool isPrime = 1 ;
+
+    for(int i = 2; i<n; i++) {
+
+        //rem = 0, Not a Prime
+        if(n%i == 0) {
+            //cout<<" Not a Prime Number" << endl;
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if(isPrime == 0) {
+        cout<<" Not a Prime Number"<<endl;
+    }
+    else
+    {
+        cout<<"is a Prime Number "<<endl;
+    }
+
+    return 0;
+}
+
+
+
+// Question Links:
+// - Count primes:https://leetcode.com/problems/count-p...
+ 
+// - Modular Exponentiation: https://bit.ly/3peOT9i
+ 
+
