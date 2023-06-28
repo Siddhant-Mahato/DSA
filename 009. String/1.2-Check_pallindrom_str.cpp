8dee8e5a990deafc93da61dfb794b6bea0628cd6
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-06-29 00:46:44 +0530

    String

---
+++ 009. String/1.2-Check_pallindrom_str.cpp
@@ -0,0 +1,24 @@
+
+#include <vector>
+#include <bits/stdc++.h>
+#include <stdio.h>
+
+// bool checkPalindrome(char a[], int n)
+// {
+//     int s = 0;
+//     int e = n - 1;
+
+//     while (s <= e)
+//     {
+//         if (toLowerCase(a[s]) != toLowerCase(a[e]))
+//         {
+//             return 0;
+//         }
+//         else
+//         {
+//             s++;
+//             e--;
+//         }
+//     }
+//     return 1;
+// }
