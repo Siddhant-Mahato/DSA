5d9ea5f8061fe3dc7ff9f7c7e524a56008894247
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-07-08 16:23:44 +0530

    Recursion-2

---
+++ 016. Recursion-2/1.3-Climb-stairs.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+
+int countDistinctWayToClimbStair(long long nStairs)
+{
+    //base case
+    if(nStairs < 0)
+        return 0;
+    
+    if(nStairs == 0)
+        return 1;
+    
+    //R.C
+    int ans = countDistinctWayToClimbStair(nStairs-1) 
+        + countDistinctWayToClimbStair(nStairs-2);
+    
+    return ans;
+}
