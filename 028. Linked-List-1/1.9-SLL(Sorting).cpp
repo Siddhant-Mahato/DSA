ada3f570e328d2b14cdda5001a90c881b7d770ee
Siddhant Mahato <127202564+Siddhant-Mahato@users.noreply.github.com>
2023-08-03 00:09:33 +0530

    SLL(Sorting)

---
+++ 028. Linked-List-1/1.9-SLL(Sorting).cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+
+/*
+
+void sortLinkedList(Node *&head)
+{
+    vector<int> elements;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        elements.push_back(temp->data);
+        temp = temp->next;
+    }
+
+    sort(elements.begin(), elements.end());
+
+    temp = head;
+    for (size_t i = 0; i < elements.size(); i++)
+    {
+        temp->data = elements[i];
+        temp = temp->next;
+    }
+}
+
+
+*/
